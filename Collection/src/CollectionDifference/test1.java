eb1b3aab314d7d224261518944bca349bec18b8b
Shree <Shree@DESKTOP-0QVUMA9>
2024-06-04 20:54:36 +0530

    test program updated

---
+++ Collection/src/CollectionDifference/test1.java
@@ -0,0 +1,13 @@
+package CollectionDifference;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	 System.out.println("test Program added");
+	 }
+
+	
+
+}
